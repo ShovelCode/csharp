7bc7c626e46a5ed2128aa66b842768e82d4d47ef
Christopher Ray <vde435@gmail.com>
2022-10-04 19:33:18 -0400

    Create helloworld.cs

---
+++ mslearn/helloworld.cs
@@ -0,0 +1,6 @@
+// Console.WriteLine("Hello World!");
+
+//This prints a single line of output
+Console.Write("Congratulations!");
+Console.Write(" ");
+Console.Write("You wrote your first lines of code!");
